6e9d62484ee18a263856a039d269a20c0d91edaa
KevinMorales <rm8206304@gmail.com>
2026-06-20 19:05:00 -0300

    Deteccion y correcion de error de diseño: detecte que la imagen que servia de fondo para la pagina principal no se cargaba correctamente; tras una  investigacion el origen del error, fue no haber importado correctamente en el proyecto, por lo que al desplegarse el proyecto, el codigo no encontraba la imagen, por lo que importamos la imagen en el codigo principal y ahora deberia funcionar. Fin del commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import type { CounterStats } from './types';
+import backgroundImage from './assets/background.jpg';
 
 // Definimos la interfaz para el estado del tiempo
 interface TimeLeft {
@@ -73,7 +74,7 @@ function App() {
   return (
     <div 
       className="relative min-h-screen bg-cover bg-center bg-no-repeat text-white font-sans selection:bg-pink-500"
-      style={{ backgroundImage: `url('./src/assets/background.jpg')` }} 
+      style={{ backgroundImage: `url(${backgroundImage})` }} 
     >
       {/* Capa de superposición oscura y opaca */}
       <div className="absolute inset-0 bg-black/60 z-0"></div>
